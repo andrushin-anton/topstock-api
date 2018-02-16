aac85c7f636175f6eb96c861f87b1521d422de59
Anton Andrushin <andrushin.anton@gmail.com>
2018-02-17 00:08:51 +0200

    update price

--- app/models/api/v1/company.rb
+++ app/models/api/v1/company.rb
@@ -48,20 +48,30 @@ class Api::V1::Company < ApplicationRecord
   def self.create_company_if_not_exists_from_csv_line(csv_line, exchange_name)
     # ignore if sector is n/a or empty - because it is not a company
     unless csv_line[6].downcase == 'n/a' || csv_line[6] == ''
-      begin
-        Api::V1::Company.create!(
-            :ticker => csv_line[0],
-            :name => csv_line[1],
-            :price => csv_line[2],
-            :exchange => exchange_name,
-            :sector => csv_line[6],
-            :industry => csv_line[7],
-            :status => self::STATUS_NEED_STATS,
-            :rank => '0'
-        )
-        puts csv_line[0] + ' created!'
-      rescue ActiveRecord::RecordNotUnique
-        # The company already exists.
+      company = Api::V1::Company.where('ticker = ?', csv_line[0]).first
+
+      unless company.nil?
+        # update
+        company.ticker = csv_line[0]
+        company.name = csv_line[1]
+        company.price = csv_line[2]
+        company.exchange = exchange_name
+        company.sector = csv_line[6]
+        company.industry = csv_line[7]
+        company.status = company.status == self::STATUS_NEED_STATS ? self::STATUS_NEED_STATS : self::STATUS_NEED_CALCULATIONS
+        company.save
+        puts "#{csv_line[0]} updated!"
+      else
+        company = Api::V1::Company.new
+        company.name = csv_line[1]
+        company.price = csv_line[2]
+        company.exchange = exchange_name
+        company.sector = csv_line[6]
+        company.industry = csv_line[7]
+        company.status = self::STATUS_NEED_STATS
+        company.rank = '0'
+        company.save
+        puts "#{csv_line[0]} created!"
       end
     end
   end
