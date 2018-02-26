57b684dd019674928896b013835e845ebd35faca
Anton Andrushin <andrushin.anton@gmail.com>
2018-02-27 01:39:35 +0200

    updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,13 @@ Rails.application.routes.draw do
 
   root 'home#index'
 
-  constraints subdomain: 'api' do
+  #constraints subdomain: 'api' do
     scope module: 'api' do
       namespace :v1 do
         get 'companies' => 'companies#index'
         get 'companies/:ticker' => 'companies#ticker'
       end
     end
-  end
+  #end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
