9d15a9906bc462cc0fea0bcb1562eb0746e8bb60
Anton Andrushin <andrushin.anton@gmail.com>
2018-02-27 01:34:10 +0200

    add root route

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,7 @@
+class HomeController < ApplicationController
+
+  def index
+    render html: "TopStock App"
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+
+  root 'home#index'
+
   constraints subdomain: 'api' do
     scope module: 'api' do
       namespace :v1 do
