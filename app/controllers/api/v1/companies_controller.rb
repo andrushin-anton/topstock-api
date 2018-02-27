b44c5793200e22b17327e138bbc499f45a60362a
Anton Andrushin <andrushin.anton@gmail.com>
2018-02-27 12:49:59 +0200

    add public cahche

--- app/controllers/api/v1/companies_controller.rb
+++ app/controllers/api/v1/companies_controller.rb
@@ -2,13 +2,15 @@ class Api::V1::CompaniesController < ApplicationController
 
   # GET /api/v1/companies
   def index
-    @api_v1_companies = Api::V1::Company.order('rank DESC')
+    expires_in 3.minutes, public: true
 
+    @api_v1_companies = Api::V1::Company.order('rank DESC')
     paginate json: @api_v1_companies
   end
 
   # GET /api/v1/companies/:ticker
   def ticker
+    expires_in 3.minutes, public: true
     render json: Api::V1::Company.find_by_ticker(params[:ticker])
   end
 
