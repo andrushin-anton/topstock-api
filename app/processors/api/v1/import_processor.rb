ba4931c82624eab34d89617e8602a2d54f5d86aa
Anton Andrushin <andrushin.anton@gmail.com>
2018-03-03 16:28:04 +0200

    make https requests for importing

--- app/processors/api/v1/import_processor.rb
+++ app/processors/api/v1/import_processor.rb
@@ -15,19 +15,19 @@ class ImportProcessor
 
   def run()
     # Add companies from AMEX
-    companies = Utilities::read_remote_csv('http://www.nasdaq.com/screening/companies-by-industry.aspx?exchange=AMEX&render=download')
+    companies = Utilities::read_remote_csv('https://www.nasdaq.com/screening/companies-by-industry.aspx?exchange=AMEX&render=download')
     companies.each do |line|
       Api::V1::Company::create_company_if_not_exists_from_csv_line(line, 'AMEX')
     end
 
     # Add companies from NASDAQ
-    companies = Utilities::read_remote_csv('http://www.nasdaq.com/screening/companies-by-industry.aspx?exchange=NASDAQ&render=download')
+    companies = Utilities::read_remote_csv('https://www.nasdaq.com/screening/companies-by-industry.aspx?exchange=NASDAQ&render=download')
     companies.each do |line|
       Api::V1::Company::create_company_if_not_exists_from_csv_line(line, 'NASDAQ')
     end
 
     # Add companies from AMEX
-    companies = Utilities::read_remote_csv('http://www.nasdaq.com/screening/companies-by-industry.aspx?exchange=NYSE&render=download')
+    companies = Utilities::read_remote_csv('https://www.nasdaq.com/screening/companies-by-industry.aspx?exchange=NYSE&render=download')
     companies.each do |line|
       Api::V1::Company::create_company_if_not_exists_from_csv_line(line, 'NYSE')
     end
